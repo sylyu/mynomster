e4b724f2b6f6d240f058ad39d0ff530dd85e43df
Sylyu <sylyu>
2016-10-17 21:17:32 +0000

    Activate link in top navigatin

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ end
 
 # Include bootstrap
 gem 'bootstrap-sass', '~>3.3.5.1'
+gem 'simple_form'
 gem 'sprockets', '2.11.0'
 
 gem 'will_paginate'
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,4 +2,20 @@ class PlacesController < ApplicationController
   def index
     @places = Place.all.page(params[:page]).per_page(3)
   end
+
+  def new
+    @place = Place.new
+  end
+
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
+
 end
