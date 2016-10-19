6fcb7d05c569c21b9c8ac961d0a53c5d2bed94c6
Sylyu <sylyu>
2016-10-19 00:30:54 +0000

    Active controller exception caught. couldn't find place id 1

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -14,10 +14,13 @@ class PlacesController < ApplicationController
     redirect_to root_path
   end
 
+  def show
+    @place = Place.find(params[:id])
+  end
+
   private
 
   def place_params
     params.require(:place).permit(:name, :description, :address)
   end
-
 end
