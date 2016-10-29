5eeb01df86fae0f48efe7d5403f9e2be798cd648
Sylyu <sylyu>
2016-10-29 16:32:46 +0000

    deleted duplicate column in migration file

--- db/migrate/20161026212245_add_picture_to_photo.rb
+++ db/migrate/20161026212245_add_picture_to_photo.rb
@@ -1,7 +1,7 @@
 class AddPictureToPhoto < ActiveRecord::Migration
   def change
     add_column :photos, :picture, :string
-    add_column :photos, :caption, :text
+    #add_column :photos, :caption, :text
 
   end
 end
