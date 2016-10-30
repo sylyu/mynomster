fa37f7bcda8ddcfaeb49da03d21a4840efd7a06f
Sylyu <sylyu>
2016-10-30 22:06:03 +0000

    created sent email from development environment

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,8 +2,8 @@ class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-    mail(to:"sylviah.yu@gmail.com", 
-      subject: "A comment has been added to your place")
+    mail(to: "sylviayutest@gmail.com",
+         subject: "A comment has been added to your place")
   end
     
 end
