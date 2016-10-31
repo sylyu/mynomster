2f0dc20695cfb6085195e96d46a39c8f4edbd74e
Sylyu <sylyu>
2016-10-31 22:45:14 +0000

    commented out triggered email temporarily

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
-
+  after_create :send_comment_email
 
   RATINGS = {
     'one star'    => '1_star',
@@ -14,5 +14,7 @@ class Comment < ActiveRecord::Base
     RATINGS.invert[self.rating]
   end
 
-
+  def send_comment_email
+    NotificationMailer.comment_added(self).deliver
+  end
 end
