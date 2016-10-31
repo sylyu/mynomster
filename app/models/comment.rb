1db34fe7517abc900ce5c4255c76b140c1327452
Sylyu <sylyu>
2016-10-31 21:59:35 +0000

    Adjusted auto emailer to send from comments to real account

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
 
+
   RATINGS = {
     'one star'    => '1_star',
     'two stars'   => '2_stars',
@@ -12,4 +13,6 @@ class Comment < ActiveRecord::Base
   def humanized_rating
     RATINGS.invert[self.rating]
   end
+
+
 end
