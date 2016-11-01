343c9a8c2dd4ac21daa9bffd971c34a73d45bf7d
Sylyu <sylyu>
2016-11-01 04:30:52 +0000

    fix errors

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ Nomster::Application.configure do
 #    password: ENV['GMAIL_PASSWORD']
 #  }
 
-  config.action_mailer.delivery_method = test
+#  config.action_mailer.delivery_method = test
 
   config.action_mailer.default_url_options = { host: 'localhost:3030' }
   # Settings specified here will takconfig.action_mailer.default_url._options = { host: 'https://nomster-sylvia-yu.herokuapp.com' }e precedence over those in config/application.rb.
