7cf71310ec3190381d84e5f8c2e851e692fa4fc7
Sylyu <sylyu>
2016-11-01 00:26:48 +0000

    revert to previous submission

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,17 +1,17 @@
 Nomster::Application.configure do
-  config.action_mailer.delivery_method = :smtp
+#config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "example.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV['GMAIL_ADDRESS'],
-    password: ENV['GMAIL_PASSWORD']
-  }
+#  config.action_mailer.smtp_settings = {
+#    address: "smtp.gmail.com",
+#    port: 587,
+#    domain: "example.com",
+#    authentication: "plain",
+#    enable_starttls_auto: true,
+#    user_name: ENV['GMAIL_ADDRESS'],
+#    password: ENV['GMAIL_PASSWORD']
+#  }
 
-#  config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = test
 
   config.action_mailer.default_url_options = { host: 'localhost:3030' }
   # Settings specified here will takconfig.action_mailer.default_url._options = { host: 'https://nomster-sylvia-yu.herokuapp.com' }e precedence over those in config/application.rb.
